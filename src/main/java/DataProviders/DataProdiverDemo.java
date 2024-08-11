05b51600105cbf824a8d45fc5ee1e6de4ceef53d
Bhumika Shrestha <66097955+Bhumicha@users.noreply.github.com>
2024-08-11 22:28:51 +0545

    Create DataProdiverDemo.java

---
+++ src/main/java/DataProviders/DataProdiverDemo.java
@@ -0,0 +1,58 @@
+package DataProviders;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DataProdiverDemo {
+    WebDriver driver;
+
+    @BeforeClass
+    void setUp()
+    {
+        driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @Test
+    void testLogin() throws InterruptedException {
+        driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("bhumicha611@gmail.com");
+        driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("password");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        Thread.sleep(2000);
+        boolean status=driver.findElement(By.xpath("//h2[normalize-space()='My Account']")).isDisplayed();
+        if (status==true)
+        {
+            driver.findElement(By.xpath("//a[@class='list-group-item'][normalize-space()='Logout']")).click();
+            Assert.assertTrue(true);
+        }
+        else
+        {
+            Assert.fail();
+        }
+    }
+    @AfterClass
+    void tearDown()
+    {
+        driver.close();
+    }
+
+    @DataProvider
+    String [][] loginData()
+    {
+        String data[][]={
+                {"abs@gmail.com", "test123"},
+                {"bhumicha611@gmail", "password"}
+
+        };
+        return data;
+    }
+}
