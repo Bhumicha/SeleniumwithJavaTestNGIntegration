a847f2100435d3798686cc8df9436f421af5e782
Bhumika Shrestha <66097955+Bhumicha@users.noreply.github.com>
2024-08-11 21:54:52 +0545

    Add files via upload

--- src/main/java/GroupDedendencyTest/LoginTests.java
+++ src/main/java/GroupDedendencyTest/LoginTests.java
@@ -1,2 +1,21 @@
-package GroupDedendencyTest;public class LoginTests {
+package GroupDedendencyTest;
+
+import org.testng.annotations.Test;
+
+public class LoginTests {
+    @Test(priority = 1, groups = {"sanity"})
+    void loginByEmail()
+    {
+        System.out.println("This is login by email");
+    }
+    @Test(priority = 2, groups = {"sanity"})
+    void loginByFacebook()
+    {
+        System.out.println("This is login by Facebook");
+    }
+    @Test(priority = 3, groups = {"sanity"})
+    void loginByTwitter()
+    {
+        System.out.println("This is login by Twitter");
+    }
 }
